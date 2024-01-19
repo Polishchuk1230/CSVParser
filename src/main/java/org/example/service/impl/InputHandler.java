bd8680ebb1f71a3f1c323b49bd5ab86d2f0b018a
Ihor Polishchuk <ihor@MacBook-Air-Ihor.local>
2024-01-19 14:53:07 +0200

    fixed bug when we can't provide more than one parameter for a command

--- src/main/java/org/example/service/impl/InputHandler.java
+++ src/main/java/org/example/service/impl/InputHandler.java
@@ -3,6 +3,7 @@ package org.example.service.impl;
 import static org.example.exception.ExceptionMessage.WRONG_INPUT_MESSAGE;
 
 import java.io.BufferedReader;
+import java.util.Arrays;
 import java.util.List;
 import org.example.dto.FileContentDto;
 import org.example.service.IExceptionHandler;
@@ -13,7 +14,7 @@ public class InputHandler implements org.example.service.IInputHandler {
   public static final String USER_INPUT_VALUE_SEPARATOR = " "; // space
   public static final int FILE_NAME_POSITION_NUMBER = 1;
   public static final int ACTION_TYPE_POSITION_NUMBER = 2;
-  public static final int ACTION_PARAMETER_POSITION_NUMBER = 3;
+  public static final int ACTION_PARAMETERS_POSITION_NUMBER = 3;
 
   private final IExceptionHandler IExceptionHandler;
   private final InputValidator inputValidator;
@@ -35,13 +36,12 @@ public class InputHandler implements org.example.service.IInputHandler {
 
     String pathToFile = splitInput[FILE_NAME_POSITION_NUMBER];
     String actionName = splitInput[ACTION_TYPE_POSITION_NUMBER];
-    String parameter = splitInput[ACTION_PARAMETER_POSITION_NUMBER];
+    String[] parameters = Arrays.copyOfRange(splitInput, ACTION_PARAMETERS_POSITION_NUMBER, splitInput.length);
 
     List<String> processedData;
     try {
       FileContentDto<BufferedReader> fileContentDto = IUploadService.uploadFile(pathToFile);
-      processedData = new ActionDispatcher(fileContentDto)
-          .dispatchAction(actionName, parameter);
+      processedData = new ActionDispatcher(fileContentDto).dispatchAction(actionName, parameters);
     } catch(Exception e) {
       processedData = IExceptionHandler.handleException(e);
     }
